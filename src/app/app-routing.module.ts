205b188a78b7c61493445cff340bd60656ea6792
nataliatrocha <natalia1tj@gmail.com>
2022-11-25 11:21:11 +0100

    feat: lesson 4 public holidays

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,15 +3,18 @@ import { RouterModule } from '@angular/router';
 import { ProductListComponent } from './components/product-list/product-list.component';
 import { CategoriesListComponent } from './components/categories-list/categories-list.component';
 import { CryptoListComponent } from './components/crypto-list/crypto-list.component';
+import { PublicHolidaysListComponent } from './components/public-holidays-list/public-holidays-list.component';
 import { ProductListComponentModule } from './components/product-list/product-list.component-module';
 import { ProductsServiceModule } from './services/products.service-module';
 import { CategoriesListComponentModule } from './components/categories-list/categories-list.component-module';
 import { CategoriesServiceModule } from './services/categories.service-module';
 import { CryptoListComponentModule } from './components/crypto-list/crypto-list.component-module';
 import { CryptoChipsServiceModule } from './services/crypto-chips.service-module';
+import { PublicHolidaysListComponentModule } from './components/public-holidays-list/public-holidays-list.component-module';
+import { PublicHolidaysServiceModule } from './services/public-holidays.service-module';
 
 @NgModule({
-  imports: [RouterModule.forRoot([{ path: 'products', component: ProductListComponent }, { path: 'categories', component: CategoriesListComponent }, { path: 'crypto', component: CryptoListComponent }]), ProductListComponentModule, ProductsServiceModule, CategoriesListComponentModule, CategoriesServiceModule, CryptoListComponentModule, CryptoChipsServiceModule],
+  imports: [RouterModule.forRoot([{ path: 'products', component: ProductListComponent }, { path: 'categories', component: CategoriesListComponent }, { path: 'crypto', component: CryptoListComponent }, { path: 'public-holidays', component: PublicHolidaysListComponent }]), ProductListComponentModule, ProductsServiceModule, CategoriesListComponentModule, CategoriesServiceModule, CryptoListComponentModule, CryptoChipsServiceModule, PublicHolidaysListComponentModule, PublicHolidaysServiceModule],
   exports: [RouterModule]
 })
 export class AppRoutingModule {
---
+++ src/app/models/public-holidays.model.ts
@@ -0,0 +1,3 @@
+export interface PublicHolidaysModel {
+  readonly localName: string;
+}
